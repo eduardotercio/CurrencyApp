cd093af996288964fdacd86bd6db367f8e377f78
edu37 <dudubezerra33@hotmail.com>
2024-06-12 00:10:17 -0300

    Format fix

--- composeApp/src/commonMain/kotlin/util/AmountFormat.kt
+++ composeApp/src/commonMain/kotlin/util/AmountFormat.kt
@@ -3,10 +3,9 @@ package util
 fun String.formatCurrency(): String {
     val parts = this.split(".")
     val integerPart = parts[0]
-    val fractionalPart = parts[1]
+    val fractionalPart = if (parts.size > 1) parts[1].padEnd(2, '0') else "00"
 
-    val formattedFractionalPart = fractionalPart.padEnd(2, '0')
     val formattedIntegerPart = integerPart.reversed().chunked(3).joinToString(".").reversed()
 
-    return "$formattedIntegerPart.$formattedFractionalPart"
+    return "$formattedIntegerPart.$fractionalPart"
 }
